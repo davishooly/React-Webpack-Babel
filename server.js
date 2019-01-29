43ea9d282fc6e33fe9a2eac9667e8cff250b9f3e
daviskimame <hoolykim@gmail.com>
2019-01-29 12:50:36 +0300

    refactored code

--- server.js
+++ server.js
@@ -1,13 +1,13 @@
 const express = require("express");
 const path = require("path");
-const port = process.env.PORT || 8080;
+const port = process.env.PORT || 3000;
 const app = express();
 
 app.use(express.static(__dirname));
-app.use(express.static(path.join(__dirname, "dist")));
+app.use(express.static(path.join(__dirname + "/dist")));
 
 app.get("/*", (req, res) => {
-  res.sendFile(path.resolve(__dirname, "dist", "index.html"));
+  res.sendFile(path.resolve(__dirname, "index.html"));
 });
 
 app.listen(port);
